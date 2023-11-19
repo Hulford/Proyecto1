ed9b2eb4da72694575467e1715574b83f4ab5de3
Hulford <moraleshulford@gmail.com>
2023-11-18 22:22:22 -0500

    Tarea sobre Modulos

--- src/app/counter/components/counter.module.ts
+++ src/app/counter/components/counter.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [CounterComponent]
+  declarations: [CounterComponent],
+  exports: [CounterComponent]
 })
 export class CounterModule { }
