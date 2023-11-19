2de00284910ab0ac5c2e7a1adcdee3161b02c9e2
Hulford <moraleshulford@gmail.com>
2023-11-19 04:56:10 -0500

    Resolucion de la tarea

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -14,10 +14,12 @@ public characterList: Character[]=[{
 }]
 
 @Output()
-public onDelete: EventEmitter<number> = new EventEmitter();
+public onDelete: EventEmitter<String> = new EventEmitter();
 
-onDeleteCharacter(index: number):void{
+onDeleteCharacter(id?: string):void{
   // Todo emitir el id del personaje
-  this.onDelete.emit(index);
+if (!id)return;
+
+  this.onDelete.emit(id);
 }
 }
