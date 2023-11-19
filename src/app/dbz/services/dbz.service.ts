1e4f91a0732f6afea58afb681fc56cf693de0077
Hulford <moraleshulford@gmail.com>
2023-11-19 04:43:23 -0500

    Paquetes externos - UUIP

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,29 +1,46 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid} from 'uuid'
+uuid()
+console.log
+
 import { Character } from '../interfaces/character.interfase';
+
+
 
 @Injectable({providedIn: 'root'})
 
 export class DbzService {
   public characters: Character[]=[{
+    id: uuid(),
     name:'Krilin',
     power:1000
   },{
+    id: uuid(),
     name:'Goku',
     power:9500
   },{
+    id: uuid(),
     name:'Vegetta',
     power:7500
   }];
 
   onNewCharacter(character:Character):void{
-  this.characters.push(character);
+  const newCharacter: Character={
+    id:uuid(), ...character
+
+
+  };
+
+    this.characters.push(newCharacter);
   //characters.push agregar al final del arreglo
   //characters.unshift agregar arriba del arreglo
   }
-  onDeleteCharacter(index: number){
-    this.characters.splice(index, 1);
-  }
+  // onDeleteCharacter(index: number){
+  //   this.characters.splice(index, 1);
+  // }
 
-
+deleteCharacterById(id:String){
+  this.characters= this.characters.filter(character => character.id !== id);
+}
 
 }
